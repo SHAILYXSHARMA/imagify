60c38f2d2fc7044a6ad8c0423b50bae7bd10654b
SHAILYXSHARMA <shailysharma142003@gmail.com>
2025-06-10 17:07:38 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react'
+import {Routes,Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css'
+
+
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import { AppContext } from './context/AppContext'
+
+<img src="/favicon.svg" alt="favicon Logo" />
+
+
+const App = () => {
+
+ const {showLogin}=useContext(AppContext)
+
+
+  return (
+    <div className='px-4 sm:px-10 md:px14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <ToastContainer position ='bottom-right'/>
+      <Navbar/>
+      {showLogin && <Login/>}
+       <Routes>
+         <Route path='/' element ={<Home/>}/>
+         <Route path='/buycredit' element ={<BuyCredit/>}/>
+         <Route path='/result' element ={<Result/>}/>
+
+       </Routes>
+       <Footer/>
+     
+    </div>
+  )
+}
+
+export default App
