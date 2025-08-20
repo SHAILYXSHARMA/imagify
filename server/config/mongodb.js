647a09d90122896985a34c9c074278c8487f505f
SHAILYXSHARMA <shailysharma142003@gmail.com>
2025-08-21 00:30:35 +0530

    fix: updated mongodb connection for render

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
-const connectDB =async ()=>{
-    
-    mongoose.connection.on('connected',()=>{
-        console.log("Databse Connected")
-    })
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
-}
-
-
-
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "imagify", // specify db name here instead of appending
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
