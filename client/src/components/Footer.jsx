b35ea017f36af94d814decfa6ab45f7179076599
SHAILYXSHARMA <shailysharma142003@gmail.com>
2026-07-13 12:50:14 +0530

    changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-center gap-4 py-3 mt-20'>
      <img src={assets.logo} alt="" width={150} />
-     <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @SHaily | All right resereved.</p>
+     <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright | All rights reserved.</p>
      <div className='flex gap-2.5'>
         <img src={assets.facebook_icon} alt="" width={35}/>
         <img src={assets.instagram_icon} alt="" width={35}/>
